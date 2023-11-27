829460f4bc9544497a3ad937e6b74ec8aad6e43e
jiwon <jiwon0768@tukorea.ac.kr>
2023-11-27 16:09:38 +0900

    전체 수정

--- client.py
+++ client.py
@@ -1,54 +1,51 @@
 # -*- coding: utf-8 -*-
 import socket
-
+import random
 
 HOST = '127.0.0.1'
-
 PORT = 9999
 
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sender_socket.bind((HOST, PORT))
+sender_socket.listen()
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+previous_ack = None # 이전에 전송한 ack를 저장할 변수
+expected_packet = 100 # 다음으로 수신되어야 할 패킷 초기화
 
-client_socket.connect((HOST, PORT))
+while True:
+    client_socket, addr = sender_socket.accept()
 
+    try:
+        #패킷 수신
+        data = client_socket.recv(4)
+        length = int.from_bytes(data, "little")
+        data = client_socket.recv(length)
+        msg = data.decode()
+        print('----------> ', msg)
 
-for i in range(3):
+        if data == (expected_packet).to_bytes(4, byteorder="little"):
+            if previous_ack is None:
+                   ack_packet = f'ACK {expected_packet}'
+                    
+            else:
+                # ACK 송신
+                ack_packet = msg + ' ACK'
+        else:
+            # 패킷이 올바르게 수신되지 않은 경우, 이전에 올바르게 수신된 패킷의 ack를 재전송
+            ack_packet = previous_ack
 
-    # 패킷 요청
-    packet = str(100 + i)
+        ack_data = ack_packet.encode()
+        ack_length = len(ack_data)    
 
-    # msg = '------------>  packet ' + packet +' send'
-    msg = packet
-    
-    data = msg.encode()
+        client_socket.sendall(ack_length.to_bytes(4, byteorder="little"))
+        client_socket.sendall(ack_data)
+        print('<---- ', ack_packet, 'send')
 
-    length = len(data)
+        # 다음으로 수신되어야 할 패킷
+        expected_packet += 1
+                     
 
-    client_socket.sendall(length.to_bytes(4, byteorder="little"))
-
-    client_socket.sendall(data)
-
-    data = client_socket.recv(4)
-
-    length = int.from_bytes(data, "little")
-
-    data = client_socket.recv(length)
-
-    msg = data.decode()
-
-    print(msg)
-
-    # ack 송신
-    ack = 'ACK' + packet
-    
-    data = ack.encode()
-
-    length = len(data)
-
-    client_socket.sendall(length.to_bytes(4, byteorder="little"))
-
-    client_socket.sendall(data)
-
-    print('<---- ', ack, 'send')
-
-client_socket.close()
+    except Exception as e:
+        print("Exception : ", e)
+    finally:
+        client_socket.close()
--- server.py
+++ server.py
@@ -1,65 +1,112 @@
 # -*- coding: utf-8 -*-
 import socket, threading
+import time # 타임아웃을 위해 time 모듈 추가
+import random
 
-def binder(client_socket, addr):
+#초기 설정
+MSS = 1024 # Maximum Segment Size
+congestion_window = 1 # 초기 혼잡 윈도우 크기
+threshold = 6 # 초기 임계값
+SS_THRESH = threshold # 중복 ACK 이벤트 발생 시 감소할 임계값 초기화
+timeout_duration = 0.1 # Timeout이 발생할 시간(초)
+PKT_NUM = 100 # 패킷 번호 초기화
+transmission_rate = 1 # 전송률
+
+
+#혼잡 윈도우 및 임계값 조절 함수
+def update_cwnd():
+    global congestion_window, threshold
+    if congestion_window < threshold:
+        congestion_window *= 2 # 임계값 도달 전에는 두배씩 지수적으로 증가시킴
+    else:
+        congestion_window += 1 # 임계값 도달 시 + 1씩 선형적으로 증가시킴
+
+# 패킷 생성 함수
+def make_packet():
+    global PKT_NUM
+    packet = PKT_NUM + 1
+    return str(packet)
+    
+def sender(client_socket, addr):
+    global PKT_NUM, congestion_window
 
     print('Connected by', addr)
 
     try:
-        
-        while True:
-           
-            data = client_socket.recv(4)
-            
-            length = int.from_bytes(data, "little")
-            
-            data = client_socket.recv(length)
-            
-            msg = data.decode()
-            
-            # print('Received from', addr)
-            print('------------> ', msg)
+        for i in range(20):
+            # 패킷 생성
+            packet = str(100 + i)
+            send_msg = 'packet ' + packet + ' send'
+
+            if random.random() < 0.3: # 20%의 확률로 패킷 손실
+                print('Packet lost!')
+                continue # 패킷 손실 시 다음 패킷으로 이동
+
+            # Timeout을 일으키기 위해 ACK 대기 시간을 짧게 설정함
+            client_socket.settimeout(timeout_duration)
 
             # 패킷 송신
-            msg = msg + 'packet received'
-
-            data = msg.encode()
-
-            # msg = '------------>  packet received'
-            
-            # data = msg.encode()
-            
+            data = packet.encode()
             length = len(data)
-            
             client_socket.sendall(length.to_bytes(4, byteorder="little"))
-            
             client_socket.sendall(data)
-    except:
+            print('----------> ' + send_msg)
+
+            # 패킷 전송 간격 조절
+            time.sleep(transmission_rate)
         
-        print("except : ", addr)
+
+            try:
+                # 수신 ACK 처리
+                ack_data = client_socket.recv(4)
+                ack_length = int.from_bytes(ack_data, "little")
+                ack_data = client_socket.recv(ack_length)
+                ack_msg = ack_data.decode()
+
+                # 중복 ACK 이벤트 발생 시
+                if "duplicate ACK" in ack_msg:
+                    print("Duplicate ACK event occurred!")
+                    SS_THRESH = congestion_window / 2  # 이벤트가 발생한 타이밍에서의 혼잡 윈도의 크기의 절반으로 설정
+                    threshold = SS_THRESH
+                    congestion_window /= 2  # 혼잡 윈도의 값을 절반으로 감소시킴
+
+                    # 중복된 3번의 ACK를 감지하고 해당 패킷을 재전송
+                    for _ in range(3):
+                        client_socket.sendall(length.to_bytes(4, byteorder="little"))
+                        client_socket.sendall(data)
+                        print('------------> ' + send_msg + ' (Retransmission)')
+
+                else:
+                    print('<---- ', ack_msg)
+                    # 혼잡 윈도우 및 임계값 조절
+                    update_cwnd()
+                    # 혼잡 윈도우 값 출력
+                    print('=> cwin 1 increase(' + str(congestion_window) + ')')
+
+            except socket.timeout:
+                print("Timeout occurred! Resending packets . . . ")
+                SS_THRESH = congestion_window / 2
+                threshold = SS_THRESH
+                congestion_window = 1
+
+                # Timeout이 발생하면 패킷 재전송함
+                for j in range(int(congestion_window)):
+                    client_socket.sendall(length.to_bytes(4, byteorder="little"))
+                    client_socket.sendall(data)
+                    j+=1
+
+            # Timeout 설정 초기화
+            client_socket.settimeout(None)
+
+    except Exception as e:
+        print("Exception:", e)
     finally:
-        
         client_socket.close()
 
+# 송신자 소켓 설정
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sender_socket.connect(('127.0.0.1', 9999))
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind(('', 9999))
-
-server_socket.listen()
-
-try:
-
-    while True:
-
-        client_socket, addr = server_socket.accept()
-
-        th = threading.Thread(target=binder, args=(client_socket,addr))
-        th.start()
-except:
-    print("server")
-finally:
-
-    server_socket.close() 
+# 수신자 측에 연결된 스레드 시작
+sender_thread = threading.Thread(target=sender, args=(sender_socket, ('127.0.0.1', 9999)))
+sender_thread.start()
